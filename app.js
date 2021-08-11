0d873fa5c0c08b668722443e8a876e6e6d65aee4
Stacey Teltser <stacey.teltser@gmail.com>
2021-08-10 20:30:03 -0500

    created constructor function that creates object for each product with name, file path, and times shown and am console logging array of products

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
 'use strict';
 console.log('this is connected');
+
+function Product(name) {
+  this.name = name;
+  this.filePath = `${name}.jpg`;
+  this.timesShown = 0;
+};
+
+const imageNames = [
+  'bag',
+  'banana',
+  'bathroom',
+  'boots',
+  'breakfast',
+  'bubblegum',
+  'chair',
+  'cthulu',
+  'dog-duck',
+  'dragon',
+  'pen',
+  'pen-sweep',
+  'scissors',
+  'shark',
+  'tauntaun',
+  'unicorn',
+  'water-can',
+  'wine-glass',
+  'sweep'
+];
+
+let products = [];
+
+for(let i = 0; i < imageNames.length; i++) {
+  products.push(new Product(imageNames[i]));
+}
+
+console.log(products);
+
